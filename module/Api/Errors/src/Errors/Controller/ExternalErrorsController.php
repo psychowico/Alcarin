dbd27688537e78d20dd652b2d53ecca600cc0f53
psychowico <psychowico@ubuntu>
2012-10-30 17:35:39 +0100

    adding requests per IP limit to external errors logging api

--- module/Api/Errors/src/Errors/Controller/ExternalErrorsController.php
+++ module/Api/Errors/src/Errors/Controller/ExternalErrorsController.php
@@ -17,9 +17,24 @@ use Zend\InputFilter\InputFilter;
 /**
  * manager external errors, probably only for debug purposes, when game will be in beta,
  * it can log players JAVASCRIPT errors from most browsers for sample.
+ * it saving incoming errors in "ext_logs" and storing requests callers ips
+ * in "ext_logs_limit" table, to make requests limit per day
+ * ( to ExternalErrorsController::LOGS_DAY_LIMIT_PER_IP )
  */
 class ExternalErrorsController extends AbstractRestfulController
 {
+    const LOGS_DAY_LIMIT_PER_IP = 5;
+
+    protected $mongo = null;
+
+    protected function mongo()
+    {
+        if( $this->mongo == null ) {
+            $this->mongo = $this->getServiceLocator()->get('mongo');
+        }
+        return $this->mongo;
+    }
+
     public function getList()
     {
         return ['getlist'=>321];
@@ -66,6 +81,7 @@ class ExternalErrorsController extends AbstractRestfulController
             ],
             [
                 'name'     => 'url',
+                'allow_empty' => true,
                 'filters'  => [
                     ['name' => 'StripTags'],
                     ['name' => 'StringTrim'],
@@ -92,27 +108,52 @@ class ExternalErrorsController extends AbstractRestfulController
 
     public function create( $data )
     {
+        //echo json_encode($data);exit;
         $inputFilter = $this->errorInputFilter();
         $inputFilter->setData( $data );
         if( $inputFilter->isValid() ) {
             $data = $inputFilter->getValues();
-
             $user_ip = $this->getRequest()->getServer('REMOTE_ADDR');
-            $data['user-ip'] = $user_ip;
 
-            $auth    = $this->getServiceLocator()->get('zfcuser_auth_service');
-            if( $auth->hasIdentity() ) {
-                $data['user-id'] = \MongoDBRef::create( 'users',
-                                new \MongoId( $auth->getIdentity()->getId() ) );
+            $mongo = $this->mongo();
+
+            $limitQ = $mongo->ext_logs_limit->findOne( ['ip' => $user_ip ] );
+            if( $limitQ == null ) {
+                $limit = 0;
+            }
+            else {
+                $limit = isset( $limitQ['count'] ) ? $limitQ['count'] : 0;
+                if( $limit > 0 && $limitQ['last_time'] instanceof \MongoDate ) {
+                    $time = $limitQ['last_time']->sec;
+                    if( (time() - $time) > ( 60 * 60 * 24 ) ) {
+                        //old limit, can be ommited
+                        $limit = 0;
+                    }
+                }
             }
 
-            //need additional validation - 5 per day per ip
+            if( $limit < static::LOGS_DAY_LIMIT_PER_IP ) {
+                //store additional information about user
+                $data['user-ip'] = $user_ip;
 
-            $mongo = $this->getServiceLocator()->get('mongo');
-            $result = $mongo->ext_logs->insert( $data );
+                $auth    = $this->getServiceLocator()->get('zfcuser_auth_service');
+                if( $auth->hasIdentity() ) {
+                    $user_id = new \MongoId( $auth->getIdentity()->getId() );
+                    $data['user-id'] = \MongoDBRef::create( 'users', $user_id );
+                }
 
-            return ['ok' => 1];
+                $mongo->ext_logs->insert( $data );
+                $this->updateLogsLimit( $user_ip, $limit + 1 );
+                return ['ok' => 1];
+            }
         }
         return ['ok' => 0];
+    }
+
+    private function updateLogsLimit( $ip, $limit )
+    {
+        $this->mongo()->ext_logs_limit->update( ['ip' => $ip],
+                ['ip' => $ip, 'count' => $limit, 'last_time' => new \MongoDate() ],
+                ['upsert' => true] );
     }
 }
--- public/js/compiled_coffee/errors-core.js
+++ public/js/compiled_coffee/errors-core.js
@@ -1,8 +1,30 @@
 
 namespace('Alcarin.JQueryPlugins', function(exports, Alcarin) {
-  var _this = this;
+  var cookie_name, day_limit;
+  cookie_name = 'js-errors';
+  /*errors nb will be stored in cookie and requests will be blocked when
+  number reach this limit
+  */
+
+  day_limit = 5;
   return this.onerror = function(msg, url, line) {
-    var data;
+    var cookie, data;
+    $.cookie.raw = true;
+    cookie = $.cookie(cookie_name);
+    if (!cookie) {
+      cookie = 0;
+      $.cookie(cookie_name, cookie, {
+        expires: 1
+      });
+    } else {
+      cookie = parseInt(cookie);
+      if (cookie >= day_limit) {
+        return false;
+      }
+      $.cookie(cookie_name, cookie + 1, {
+        expires: 1
+      });
+    }
     if (_.post) {
       data = {
         'msg': msg,
@@ -10,8 +32,8 @@ namespace('Alcarin.JQueryPlugins', function(exports, Alcarin) {
         'line': line
       };
       _.post('/api/errors/external', data);
-      return true;
     }
+    $.cookie.raw = false;
     return false;
   };
 });
